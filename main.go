588c332b36bf144ad1865451330740479bcb093a
Tomas Karasek <tom.to.the.k@gmail.com>
2017-11-22 15:58:10 +0200

    Rew

--- main.go
+++ main.go
@@ -8,26 +8,24 @@ import (
 	"github.com/t0mk/packswgo"
 )
 
-const APITokenVar = "PACKET_AUTH_TOKEN"
-
-func getCfg() *packswgo.Configuration {
-	cfg := packswgo.NewConfiguration()
-	cfg.APIKey["X-Auth-Token"] = os.Getenv(APITokenVar)
-	return cfg
-}
-
 func main() {
-	cfg := getCfg()
-
-	devicesAPI := packswgo.DevicesApi{Configuration: cfg}
+	c := packswgo.NewClient()
 	projectID := os.Getenv("PACKET_PROJECT")
 
-	devList, _, err := dAPI.FindDevices(projectID, "", 0, 10)
+	devList, _, err := c.Devices.FindDevices(projectID, "", 0, 10)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, d := range devList.Devices {
 		fmt.Println(d)
+	}
+
+	facList, _, err := c.Facilities.FindFacilities()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range facList.Facilities {
+		fmt.Println(f)
 	}
 
 }
